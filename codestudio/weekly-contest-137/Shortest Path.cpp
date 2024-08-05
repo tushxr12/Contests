e81fe6f02e753ee758244813ad76e0d75037f627
tushxr12 <tusharforcourses@gmail.com>
2024-08-05 10:17:37 +0530

    Upsolve contest problem

---
+++ codestudio/weekly-contest-137/Shortest Path.cpp
@@ -0,0 +1,25 @@
+int shortestPath(int n, string &s)
+{
+    // Write your code here.
+    int ans = INT_MAX;
+
+    int rInd = -1, bInd = -1, gInd = -1;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == 'R')
+            rInd = i;
+        else if (s[i] == 'G')
+            gInd = i;
+        else
+            bInd = i;
+
+        int mini = min({rInd, bInd, gInd});
+        if (mini >= 0)
+            ans = min(ans, i - mini);
+    }
+
+    if (ans == INT_MAX)
+        return -1;
+    return ans;
+}
