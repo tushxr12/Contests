a98863aad74106f795e2a83a8f5e1107e8ba06f3
tushxr12 <tusharforcourses@gmail.com>
2024-08-07 23:15:32 +0530

    Add: contest prob 1

---
+++ codechef/Starters_146/Olympics 2024.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // your code goes here
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int ans = (15 - (a + b + c));
+    cout << ans;
+}
