9d6c80f619aa4ae79b545484e5ba68520974aa67
tushxr12 <tusharforcourses@gmail.com>
2024-08-15 12:14:12 +0530

    Upsolve contest prob 1

---
+++ codechef/Starters_147/Gold Coins 101.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, x, y;
+    cin >> a >> b >> x >> y;
+
+    if (x > y)
+    {
+        cout << a;
+    }
+    else
+    {
+        cout << b;
+    }
+}
