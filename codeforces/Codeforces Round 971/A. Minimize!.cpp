68904ca0f0af0f60796564a078f58312bbaf9928
tushxr12 <tusharforcourses@gmail.com>
2024-09-03 23:02:15 +0530

    Codeforces contest prob 1

---
+++ codeforces/Codeforces Round 971/A. Minimize!.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve()
+{
+    // cout<<"Working";
+    int a, b;
+    cin >> a >> b;
+    cout << b - a;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+        cout << "\n";
+    }
+    return 0;
+}
