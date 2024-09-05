ad8f5070862f7976c847a497dd469ddc18af3aa6
tushxr12 <tusharforcourses@gmail.com>
2024-09-05 22:58:00 +0530

    Add contest prob 1

---
+++ codechef/Starters_150/IOI 2024.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x;cin>>x;
+    if(x >= 1 && x <= 8)
+    {
+        cout<<"YES";
+    }else
+    {
+        cout<<"NO";
+    }
+}
