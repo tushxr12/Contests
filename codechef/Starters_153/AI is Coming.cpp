d14f4749dbd90de07570eacd14c3bfba762281c9
tushxr12 <tusharforcourses@gmail.com>
2024-09-29 13:57:27 +0530

    Contest prob 1

---
+++ codechef/Starters_153/AI is Coming.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // your code goes here
+    int n;
+    cin >> n;
+    if (n <= 60)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+}
