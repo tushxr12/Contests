582b30eed184cd3d5d2bb289812c9ec410d18b57
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-01-23 15:57:28 +0530

    contest prob 1

---
+++ codechef/Starters_170/Access Code Equality.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    string s;cin>>s;
+    if(s.find("WECNITK") != string::npos)
+    {
+        cout<<"Welcome to Web Club!";
+    }else
+    {
+        cout<<"Access denied";
+    }
+}
