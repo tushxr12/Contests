e04c283cde6b541c61d98b0d4ea5ffb45891a3b8
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-02-06 17:59:22 +0530

    contest prob1 solved

---
+++ codechef/Starters_172/Time Machine.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x;cin>>x;
+    if(x == 2050)
+    {
+        cout<<"Yes";
+        return 0;
+    }
+    if(x + 25 >= 2050)
+    {
+        cout<<"Yes";
+    }
+    else
+    {
+        cout<<"No";
+    }
+}
