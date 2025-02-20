43ac78e69a5dde115e4e62bd0b90c69c821babaf
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-02-20 13:42:20 +0530

    solve contest prob 1

---
+++ codechef/Starters_174/Too Much Homework!.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x,y;cin>>x>>y;
+    if(x + y*10 >= 100)
+        cout<<"YES";
+    else
+        cout<<"NO";
+}
