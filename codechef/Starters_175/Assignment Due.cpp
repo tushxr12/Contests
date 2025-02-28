42d0b73d661faa37b763383b962a39f33934cbf4
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-02-28 13:07:42 +0530

    solve contest prob 1

---
+++ codechef/Starters_175/Assignment Due.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x,y;cin>>x>>y;
+    if(x<=y)
+        cout<<"YES";
+    else
+        cout<<"NO";
+    return 0;
+}
