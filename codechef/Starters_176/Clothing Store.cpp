cd59d6780da1f8f7697e00d4e74bd78fddf2ea45
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-03-11 17:31:21 +0530

    practice codechef problem

---
+++ codechef/Starters_176/Clothing Store.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    vector<int> s(3), t(3);
+    for(int i = 0;i < 3;i++)
+        cin>>s[i];
+    for(int i = 0;i < 3;i++)
+        cin>>t[i];
+    
+    int ans = 0;
+    for(int i = 0;i < 3;i++)
+    {
+        for(int j = 0;j <= i;j++)
+        {
+            int take = min(s[i], t[j]);
+            ans += take;
+            s[i] -= take;
+            t[j] -= take;
+        }
+    }
+    cout<<ans;
+}
+
+int main() {
+	// your code goes here
+    int t;cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+}   
