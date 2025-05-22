4e6d5cb21f9521cedd88c78b448117099b119ffb
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-05-22 12:30:00 +0530

    solve prob 1

---
+++ codechef/Starters_185/Pizza Split.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int n;cin>>n;
+    if(n%2 == 0){
+        cout<<1;
+    }
+    else
+    {
+        cout<<2;
+    }
+}
