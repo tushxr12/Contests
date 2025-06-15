2b6d46fdd17549179254fd3e1c0063a15a249e00
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-06-15 16:38:55 +0530

    upsolve prob 2 contest

--- leetcode/Weekly Contest 454/3583. Count Special Triplets.cpp
+++ leetcode/Weekly Contest 454/3583. Count Special Triplets.cpp
@@ -39,3 +39,35 @@ public:
         return cnt;
     }
 };
+
+//Upsolve 
+class Solution {
+public:
+    int specialTriplets(vector<int>& nums) {
+        int n = nums.size();
+        map<int, vector<int>> mpp;
+        int mod = 1e9 + 7;
+
+        for(int i = 0;i < n;i++)
+            mpp[nums[i]].push_back(i);
+        
+        long long ans = 0;
+
+        for(int i = 1;i < n - 1;i++)
+        {
+            int target = 2*nums[i];
+            if(!mpp.count(target))
+                continue;
+            
+            auto it = lower_bound(mpp[target].begin(), mpp[target].end(), i);
+            int before = int(it - mpp[target].begin());
+            int after = mpp[target].size() - before;
+            if(target == nums[i])
+                after--;
+            
+            ans += (long long)before*after;
+            ans %= mod;
+        }
+        return ans;
+    }
+};
