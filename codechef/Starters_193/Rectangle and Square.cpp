291e5781b60d1793391b218d9b1063d092f5a389
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-07-02 22:37:18 +0530

    upsolve contest prob 1

---
+++ codechef/Starters_193/Rectangle and Square.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int a,b,c;
+    cin>>a>>b>>c;
+    
+    if(a*b == c*c)
+    {
+        cout<<"YES";
+    }
+    else
+    {
+        cout<<"NO";
+    }
+}
