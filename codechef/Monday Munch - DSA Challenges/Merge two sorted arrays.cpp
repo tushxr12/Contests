8b8afd0dd4fbe481b2ff859fd7a3205387f05539
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-09-10 16:30:00 +0530

    upsolve contest prob

--- codechef/Monday Munch - DSA Challenges/Merge two sorted arrays.cpp
+++ codechef/Monday Munch - DSA Challenges/Merge two sorted arrays.cpp
@@ -31,3 +31,47 @@ int main() {
         cout<<i<<" ";
     return 0;
 }
+
+
+// Better
+// WIthout using extra space
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int a,b;
+    cin>>a>>b;
+    
+    vector<int> v1(a), v2(b);
+    for(int i = 0;i < a;i++){
+        cin>>v1[i];
+    }
+    
+    for(int i = 0;i < b;i++){
+        cin>>v2[i];
+    }
+
+	// Kyuki dono sorted hai so ek pointer ko 1st array ke max element pr rkho and dussre ko 2nd array ke smallest pr and then comapare them and swap if needed!
+    int left = a - 1, right = 0;
+    
+    while(left >= 0 && right < b){
+        if(v1[left] > v2[right]){
+            swap(v1[left--] , v2[right++]);
+        }
+        else
+        {
+            break;
+        }
+    }
+    
+    sort(begin(v1) , end(v1));
+    sort(begin(v2), end(v2));
+    
+    for(auto i : v1)
+        cout<<i<<" ";
+    for(auto i : v2)
+        cout<<i<<" ";
+    
+    return 0;
+}
