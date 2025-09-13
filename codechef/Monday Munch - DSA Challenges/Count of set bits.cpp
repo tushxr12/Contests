dadc69a4643a9df3e311190a859febce140179e6
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-09-13 18:21:57 +0530

    upsolve contest prob

--- codechef/Monday Munch - DSA Challenges/Count of set bits.cpp
+++ codechef/Monday Munch - DSA Challenges/Count of set bits.cpp
@@ -1,3 +1,4 @@
+// STL Approach
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -5,4 +6,24 @@ int main() {
 	// your code goes here
     int n;cin>>n;
     cout<<__builtin_popcount(n);
+}
+
+// Normal Approach
+#include <bits/stdc++.h>
+using namespace std;
+
+int countSetBits(int n)
+{
+    int cnt = 0;
+    while(n){
+        cnt += (n&1);
+        n = n >> 1;
+    }
+    return cnt;
+}
+
+int main() {
+	// your code goes here
+    int n;cin>>n;
+    cout<<countSetBits(n);
 }
