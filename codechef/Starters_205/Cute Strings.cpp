6a7c48f8cb1c90790b5e1accf8b3b76313b5738d
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-09-27 11:19:25 +0530

    upsolve contest prob 1

---
+++ codechef/Starters_205/Cute Strings.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    string s;cin>>s;
+    if(s[0] == s[2] && s[1] == 'w'){
+        cout<<"Cute";
+    }
+    else
+    {
+        cout<<"NO";
+    }
+}
