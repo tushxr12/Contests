239a3dab6b75b0d79986c03d640299bdb3eef82b
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-12-03 23:57:20 +0530

    solve contest prob

---
+++ codechef/Starters_215/Competition of 4.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x;cin>>x;
+    if(x == 1){
+        cout<<8000;
+    }
+    else if(x == 2){
+        cout<<4000;
+    }
+    else if(x == 3){
+        cout<<2000;
+    }
+    else
+    {
+        cout<<1000;
+    }
+}
