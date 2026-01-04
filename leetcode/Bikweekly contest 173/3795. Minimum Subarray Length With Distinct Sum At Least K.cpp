d5e5ff1fc8442f8c5969e21f677b303940bb7c4b
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-01-04 17:29:42 +0530

    upsolve contest prob 2

---
+++ leetcode/Bikweekly contest 173/3795. Minimum Subarray Length With Distinct Sum At Least K.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minLength(vector<int>& nums, int k) {
+        int n = nums.size();
+        long long distinctSum = 0;
+        int ans = INT_MAX;
+        int left = 0, right = 0;
+        unordered_map<int,int> mpp;
+
+        while(right < n)
+        {
+            int currVal = nums[right];
+
+            mpp[currVal]++;
+            if(mpp[currVal] == 1)
+            {
+                distinctSum += currVal;
+            }
+
+            while(distinctSum >= k)
+            {
+                ans = min(ans, right - left + 1);
+                int removeVal = nums[left];
+                mpp[removeVal]--;
+
+                if(mpp[removeVal] == 0)
+                {
+                    distinctSum -= removeVal;
+                    mpp.erase(removeVal);
+                }
+                left++;
+            }
+            right++;
+        }
+        return (ans == INT_MAX) ? -1 : ans;
+    }
+};
