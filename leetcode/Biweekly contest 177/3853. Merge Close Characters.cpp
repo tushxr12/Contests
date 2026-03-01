35a1ee044e0a2c94c1d8018c0d2c7025dcef3dca
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-03-01 21:10:14 +0530

    solve contest prob 1

---
+++ leetcode/Biweekly contest 177/3853. Merge Close Characters.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool allCharsSame(string &s)
+    {
+        set<char> st(begin(s), end(s));
+        return (st.size() == 1);
+    }
+    string mergeCharacters(string s, int k) {
+        int n = s.size();
+        string ans = "";
+        unordered_map<char,int> mpp;
+
+        if(allCharsSame(s)){
+            string ans = "";
+            ans.push_back(s[0]);
+            return ans;
+        }
+        int cnt = 0;
+        for(int i = 0;i < n;i++){
+            char currChar = s[i]; // a
+
+            if(mpp.find(currChar) != mpp.end() && ans.size() -1 -mpp[currChar] < k){
+                continue;
+            }
+            
+            ans += currChar;
+            mpp[currChar] = ans.size() - 1;
+        }
+        
+        return ans;
+    }
+};
