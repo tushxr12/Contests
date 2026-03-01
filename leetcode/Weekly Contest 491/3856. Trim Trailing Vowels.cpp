88b6ccfa3f6a7ac5e0dce681d5bbad26d2d33fe9
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-03-01 21:13:07 +0530

    solve contest prob 1

--- leetcode/Weekly Contest 491/3856. Trim Trailing Vowels.cpp
+++ leetcode/Weekly Contest 491/3856. Trim Trailing Vowels.cpp
@@ -6,12 +6,14 @@ public:
             return true;
         return false;
     }
+    
     string trimTrailingVowels(string s) {
         int n = s.size();
         for(int i = n - 1;i >= 0;i--){
             if(isVowel(s[i])){
                 s.pop_back();
             }else{
+                // break out if we encounter any non-vowel
                 break;
             }
         }
